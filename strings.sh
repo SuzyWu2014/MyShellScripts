e196bde963808c58ab6349a3ad109697c1d41ed6
shujinwu <shujinwu@oregonstate.edu>
2017-02-21 10:51:04 -0800

    update strings

--- strings.sh
+++ strings.sh
@@ -7,3 +7,6 @@ STR="locations-api-1474324986570-284c816-all.jar"
 
 echo "something-$(echo $(echo $STR) |
     awk '{split($0, subs, "-"); print subs[4]}')"
+
+EMAIL="$(git log -1 | grep -Eiorh '([[:alnum:]_.-]+@[[:alnum:]_.-]+?\.[[:alpha:].]{2,6})')"
+echo $EMAIL
---
+++ test.sh
@@ -0,0 +1,7 @@
+x=0
+
+if [[ $x != "" ]]; then
+    echo $x > txt.txt
+else
+    echo "hey"
+fi
